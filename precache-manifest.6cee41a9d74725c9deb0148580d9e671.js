5089d65650758a23aa06628c54ef0a49502d48c3
liyang-fan <876175735@qq.com>
2021-05-07 23:01:09 +0800

    deploy

---
+++ precache-manifest.6cee41a9d74725c9deb0148580d9e671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3222d31a64da0e5fffb4",
+    "url": "/person_monrey_website/css/app.d3b7daae.css"
+  },
+  {
+    "revision": "190b086fe05fe09453d388f996b41651",
+    "url": "/person_monrey_website/index.html"
+  },
+  {
+    "revision": "3222d31a64da0e5fffb4",
+    "url": "/person_monrey_website/js/app.8f04f1cf.js"
+  },
+  {
+    "revision": "2c107a3c8b8e6349083a",
+    "url": "/person_monrey_website/js/chunk-vendors.35d5f1e3.js"
+  },
+  {
+    "revision": "83d82c20a7694b90d9773fb9dc2d496f",
+    "url": "/person_monrey_website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/person_monrey_website/robots.txt"
+  }
+]);
